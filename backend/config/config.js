74291833d59c552474f2a17fa2ac66e90d50bd1b
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-11-18 11:03:46 -0500

    production 2.0

--- backend/config/config.js
+++ backend/config/config.js
@@ -8,18 +8,11 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const path = require('path');
 
 // Express Settings
 app.use(cors())
